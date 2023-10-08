3b3f9151f6732d69295f09ba785c4bee86fa9690
Eduardo DevJS <dudev2001@gmail.com>
2023-10-08 19:44:56 -0300

    ProdutoModel

--- src/MODELS/ProdutoModel.java
+++ src/MODELS/ProdutoModel.java
@@ -2,5 +2,14 @@
 package MODELS;
 
 public class ProdutoModel {
+    private String name; 
+    private String description;
+    private int price;
+    private int stockQuantity;
+    
+    public ProcessHandle(){
+        
+    }
+    
     
 }
