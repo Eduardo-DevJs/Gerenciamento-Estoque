a4ffbde9ed2516a8bbf1c321bc317eb99ee057b0
Eduardo DevJS <dudev2001@gmail.com>
2023-10-08 19:46:34 -0300

    update

--- src/MODELS/ProdutoModel.java
+++ src/MODELS/ProdutoModel.java
@@ -7,9 +7,49 @@ public class ProdutoModel {
     private int price;
     private int stockQuantity;
     
-    public ProcessHandle(){
+    public ProdutoModel(){
         
     }
+
+    public ProdutoModel(String name, String description, int price, int stockQuantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+    
     
     
 }
