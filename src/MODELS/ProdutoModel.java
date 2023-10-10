c72d1dc8c1fb8eb14e43ef187d569bf9046ea551
Eduardo DevJS <dudev2001@gmail.com>
2023-10-09 21:56:21 -0300

    Tela de Login

--- src/MODELS/ProdutoModel.java
+++ src/MODELS/ProdutoModel.java
@@ -2,6 +2,7 @@
 package MODELS;
 
 public class ProdutoModel {
+    private int id_product;
     private String name; 
     private String description;
     private int price;
@@ -11,12 +12,15 @@ public class ProdutoModel {
         
     }
 
-    public ProdutoModel(String name, String description, int price, int stockQuantity) {
+    public ProdutoModel(int id_product, String name, String description, int price, int stockQuantity) {
+        this.id_product = id_product;
         this.name = name;
         this.description = description;
         this.price = price;
         this.stockQuantity = stockQuantity;
     }
+
+    
 
     public String getName() {
         return name;
@@ -48,6 +52,14 @@ public class ProdutoModel {
 
     public void setStockQuantity(int stockQuantity) {
         this.stockQuantity = stockQuantity;
+    }
+
+    public int getId_product() {
+        return id_product;
+    }
+
+    public void setId_product(int id_product) {
+        this.id_product = id_product;
     }
     
     
